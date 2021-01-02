983a077e020304b4449d1588a9ec1e1a92fa5611
Rio_Nyx <58178550+Rio-Nyx@users.noreply.github.com>
2021-01-02 23:57:55 +0530

    Add files via upload

---
+++ src/popup.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+//      var f = d.createElement('form');
+//      f.action = 'http://gtmetrix.com/analyze.html?bm';
+//      f.method = 'post';
+//      var i = d.createElement('input');
+//      i.type = 'hidden';
+//      i.name = 'url';
+//      i.value = tab.url;
+//      f.appendChild(i);
+//      d.body.appendChild(f);
+//      f.submit();
+//    	var ur = tab.url;
+//    	var h1 = d.createElement('h1');
+    	const res = async function(){
+    	const response =await fetch(
+  "https://api.github.com/notifications", 
+  {
+    headers: {
+      authorization: "token 5c73588c6dbd89a38d84f3a70025aecd628f22d0"
+    }
+  }
+)
+	notifications = await response.json();
+	var nums = notifications.length;
+	console.log(notifications);
+	let notes = d.getElementById("notifications");
+	console.log(notifications.length);	
+	for(let i=0;i<notifications.length;i++){
+		var note = d.createElement('li');
+		note.innerHTML = notifications[i].subject.title;
+		notes.appendChild(note);
+	}
+}
+res()
+//    	h1.innerHTML = "hello";
+//    	h1.innerHTML = res.json()
+//    	d.body.appendChild(h1);
+    });
+  }, false);
+}, false);
